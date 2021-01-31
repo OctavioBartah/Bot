328df3371ee0b14c37c19ad5ac2ebd8a38aebb14
OctavioBartah <48717791+OctavioBartah@users.noreply.github.com>
2021-01-31 12:39:16 -0300

    Update index.js

--- botdukeke1Aprimored-main/index.js
+++ botdukeke1Aprimored-main/index.js
@@ -152,7 +152,7 @@ client.on('group-participants-update', async (anu) => {
 			const isCmd = body.startsWith(prefix)
 
 			mess = {
-				wait: '❬❗❭AGUARDE  ENQUANTO ESTOU PROCESSANDO❬❗❭',
+				wait: '❬❗❭AGUARDE ENQUANTO ESTOU PROCESSANDO❬❗❭',
 				success: '️❬ ✔ ❭ PRONTINHO 🖤',
 				error: {
 					stick: 'Eu falhei :( desculpa',
@@ -207,7 +207,7 @@ client.on('group-participants-update', async (anu) => {
 			if (!isCmd && isGroup) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;31mRECV\x1b[1;37m]', time, color('Message'), 'from', color(sender.split('@')[0]), 'in', color(groupName), 'args :', color(args.length))
 			switch(command) {
 				case 'help': 
-				case 'menu':
+				case 'otaku':
 					client.sendMessage(from, help(prefix), text)
 					break
 				case 'donasi':
